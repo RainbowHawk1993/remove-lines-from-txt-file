c8347e1601393e0b51b1b13ee1fd9653bd9fd75b
Nick <nicklos550@gmail.com>
2021-10-29 00:44:38 +0300

    Finds all short lines, but doesn't actually write back to file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -11,6 +12,7 @@ func main() {
 	txtFilename := flag.String("txt", "words.txt",
 		"a txt file with 1 word per line")
 	flag.Parse()
+
 	file, err := os.Open(*txtFilename)
 	if err != nil {
 		exit("Failed to open txt file.")
@@ -19,13 +21,27 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var text []string
+	//scanner.Split(bufio.ScanLines)
+
+	var bs []byte
+	buf := bytes.NewBuffer(bs)
+
+	var text string
 	for scanner.Scan() {
-		text = append(text, scanner.Text())
+		text = scanner.Text()
+		length := len(text)
+		if length > 3 {
+			_, err := buf.Write(scanner.Bytes())
+			if err != nil {
+				exit("Couldn't write line")
+			}
+			_, err = buf.WriteString("aaaa")
+			if err != nil {
+				exit("Couldn't replace line")
+			}
+		}
 	}
 }
-
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
