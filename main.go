6014f6ca0800d52e1147b2dc47a87e461af5aa43
Nick <nicklos550@gmail.com>
2021-10-28 22:32:01 +0300

    Saved input from file to array

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	txtFilename := flag.String("txt", "words.txt",
+		"a txt file with 1 word per line")
+	flag.Parse()
+	file, err := os.Open(*txtFilename)
+	if err != nil {
+		exit("Failed to open txt file.")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var text []string
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
